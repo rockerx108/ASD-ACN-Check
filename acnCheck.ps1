86f4ef855ceaeb9c95532128828a8334948730b1
Nick <nikeels92@gmail.com>
2020-09-17 14:26:38 -0600

    Add files via upload

---
+++ acnCheck.ps1
@@ -0,0 +1,33 @@
+﻿
+CLS
+
+$x = invoke-restmethod -uri  <<API Endpoint>>
+
+if($x.check -ne 1) {
+
+[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$pwd = ConvertTo-SecureString "<<Key>>" -AsPlainText -Force
+$creds = New-Object System.Management.Automation.PSCredential ("SMTP_Injection", $pwd)
+ 
+Send-MailMessage -From nkeels@kd3v.com -To <<EMAIL ADDRESS>> -Subject "ACN ISSUE" -Body "There is currently a connection issue with one of the servers. Please attend to this issue at your earliest convenience." `
+ -SmtpServer smtp.sparkpostmail.com -Port 587 -Credential $creds -UseSsl
+
+
+}else {
+$date = get-date
+
+# Create new PSobject for array values to properties
+            $asset = New-Object PSobject -Property @{
+                'Time' = $date
+                'executionTime' = $x.executionTime
+                'Check' = $x.check
+                } 
+
+
+$asset | Select-Object -property 'Time', 'Execution Time', 'Check' | export-csv -path .\checkTracker.csv
+
+ 
+}
+
+
